926fb2326ca9168272e90101bbcfa500fe28b408
Milena Hristova-Angelkova <light_milena@abv.bg>
2021-11-05 11:49:30 +0200

    Added more images. Updated Sass with mixins for responsiveness.

--- JS/data.js
+++ JS/data.js
@@ -4,27 +4,27 @@ const productsInventory = {
         "id": "5679rr",
         "price": 8.99,
         "in-stock": "available",
-        "images": ["/images/redRoses/1.jerry-klein-nAU7f7sR9eY-unsplash.jpg", "/images/redRoses/2.biel-morro-kcKiBcDTJt4-unsplash.jpg"]
+        "images": ["/images/redRoses/1.jerry-klein-nAU7f7sR9eY-unsplash.jpg", "/images/redRoses/2.biel-morro-kcKiBcDTJt4-unsplash.jpg", "/images/redRoses/3.cody-chan-OsHqxjVV0HE-unsplash.jpg"]
     },
     "pink": {
         "name": "Pink Roses",
         "id": "5679pr",
         "price": 9.99,
         "in-stock": "low stock",
-        "images": ["/images/pinkRoses/1.thais-do-rio-ABTgnOFNVYc-unsplash.jpg", "/images/pinkRoses/2.adriana-leon-3ci8WBZwH3w-unsplash.jpg"]
+        "images": ["/images/pinkRoses/1.thais-do-rio-ABTgnOFNVYc-unsplash.jpg", "/images/pinkRoses/2.adriana-leon-3ci8WBZwH3w-unsplash.jpg", "/images/pinkRoses/3.mike-castro-demaria-94BUerwdFP8-unsplash.jpg"]
     },
     "yellow": {
         "name": "Yellow Roses",
         "id": "5679yr",
         "price": 6.99,
         "in-stock": "available",
-        "images": ["/images/yellowRoses/1.kamala-bright-G_sv4-uQEbE-unsplash.jpg", "/images/yellowRoses/2.felipe-salgado-67duUrW5VaM-unsplash.jpg"]
+        "images": ["/images/yellowRoses/1.kamala-bright-G_sv4-uQEbE-unsplash.jpg", "/images/yellowRoses/2.felipe-salgado-67duUrW5VaM-unsplash.jpg", "/images/yellowRoses/3.ankit-choudhary-1ufb6oXdaYU-unsplash.jpg"]
     },
     "blue": {
         "name": "Blue Roses",
         "id": "5679br",
         "price": 15.99,
         "in-stock": "not available",
-        "images": ["/images/blueRoses/1.abhijeet-6Bjea0_-37s-unsplash.jpg", "/images/blueRoses/2.insung-yoon-iNeb6Y4KpK4-unsplash.jpg"]
+        "images": ["/images/blueRoses/1.abhijeet-6Bjea0_-37s-unsplash.jpg", "/images/blueRoses/2.insung-yoon-iNeb6Y4KpK4-unsplash.jpg", "/images/blueRoses/3.sajad-nori-jz_BDXUwKSo-unsplash.jpg"]
     }
 }
